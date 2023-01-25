cdb7a5f571d468f549595ce9379c434267bd4f8f
Demsi <wojdems@gmail.com>
2023-01-25 18:43:22 +0100

    Calculator repo

---
+++ script.js
@@ -0,0 +1,39 @@
+
+let $number1=document.getElementById(`number1`)
+let $number2=document.getElementById(`number2`)
+let $resultbox=document.getElementById(`result`)
+let $aaa=document.getElementById(`aaa`)
+let result = 0
+
+
+function plus() {
+result=parseInt($number1.value) + parseInt($number2.value)
+$resultbox.value=result
+$aaa.innerHTML+=`<p></p>` + $number1.value + `+` + $number2.value + `=` + $resultbox.value
+}
+
+function minus() {
+result=($number1.value) - ($number2.value)
+$resultbox.value=result
+$aaa.innerHTML+=`<p></p>` + $number1.value + `-` + $number2.value + `=` + $resultbox.value
+}
+
+function divide() {
+    if (parseInt($number2.value)===0){
+        console.log(`You cant divide by 0!`)
+    } else {
+        result=parseInt($number1.value) / parseInt($number2.value)
+        $resultbox.value=result
+        $aaa.innerHTML+=`<p></p>` + $number1.value + `/` + $number2.value + `=` + $resultbox.value
+    }
+}
+
+function multipla() {  
+result=parseInt($number1.value) * parseInt($number2.value)
+$resultbox.value=result
+$aaa.innerHTML+=`<p></p>` + $number1.value + `*` + $number2.value + `=` + $resultbox.value
+
+}
+
+localStorage.setItem('myCat', 'Tom')
+$aaa.innerHTML = localStorage.getItem('myCat')
