11d72ea6c65ad09f5a501b033e8d57c7dca630d4
Demsi <wojdems@gmail.com>
2023-02-01 18:32:32 +0100

    Dodano local storage, poprawki wizualne w css i html

--- script.js
+++ script.js
@@ -3,35 +3,55 @@ let $number1=document.getElementById(`number1`)
 let $number2=document.getElementById(`number2`)
 let $resultbox=document.getElementById(`result`)
 let $aaa=document.getElementById(`aaa`)
-let result = 0
 
 
 function plus() {
-result=parseInt($number1.value) + parseInt($number2.value)
-$resultbox.value=result
-$aaa.innerHTML+=`<p></p>` + $number1.value + `+` + $number2.value + `=` + $resultbox.value
+    $resultbox.value=parseInt($number1.value) + parseInt($number2.value)
+    $aaa.innerHTML+=`<p></p>` + $number1.value + `+` + $number2.value + `=` + $resultbox.value
+    updateLocalStorage($number1.value + `+` + $number2.value + `=` + $resultbox.value)
 }
 
 function minus() {
-result=($number1.value) - ($number2.value)
-$resultbox.value=result
-$aaa.innerHTML+=`<p></p>` + $number1.value + `-` + $number2.value + `=` + $resultbox.value
+    $resultbox.value=($number1.value) - ($number2.value)
+    $aaa.innerHTML+=`<p></p>` + $number1.value + `-` + $number2.value + `=` + $resultbox.value
+    updateLocalStorage($number1.value + `-` + $number2.value + `=` + $resultbox.value)
 }
 
 function divide() {
     if (parseInt($number2.value)===0){
-        console.log(`You cant divide by 0!`)
+        $aaa.innerHTML+=`<p>You can't divide by 0!</p>`
     } else {
-        result=parseInt($number1.value) / parseInt($number2.value)
-        $resultbox.value=result
+        $resultbox.value=parseInt($number1.value) / parseInt($number2.value)
         $aaa.innerHTML+=`<p></p>` + $number1.value + `/` + $number2.value + `=` + $resultbox.value
+        updateLocalStorage($number1.value + `/` + $number2.value + `=` + $resultbox.value)
     }
 }
 
 function multipla() {  
-result=parseInt($number1.value) * parseInt($number2.value)
-$resultbox.value=result
-$aaa.innerHTML+=`<p></p>` + $number1.value + `*` + $number2.value + `=` + $resultbox.value
-
+    $resultbox.value=parseInt($number1.value) * parseInt($number2.value)
+    $aaa.innerHTML+=`<p></p>` + $number1.value + `*` + $number2.value + `=` + $resultbox.value
+    updateLocalStorage($number1.value + `*` + $number2.value + `=` + $resultbox.value)
 }
 
+function updateLocalStorage(value) {
+        let tablica=[]
+    if (!localStorage.getItem(`magazyn`)) {
+        console.log(`Zrobiono magazyn`)
+        tablica.push(value)
+        localStorage.setItem(`magazyn`, JSON.stringify(tablica))
+    } else {
+        console.log(`Update magazynu`)
+        tablica = JSON.parse(localStorage.getItem(`magazyn`))
+        tablica.push(value)
+        localStorage.setItem(`magazyn`, JSON.stringify(tablica))
+        console.log(localStorage.getItem(`magazyn`))
+    } 
+}
+
+if(localStorage.getItem(`magazyn`)) {
+    let actualElements = JSON.parse(localStorage.getItem(`magazyn`))
+    
+    for(let i=0; i<actualElements.length; i++) {
+        $aaa.innerHTML += `<p>` + actualElements[i] + `</p>`
+    }
+}
