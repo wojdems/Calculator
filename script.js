6fcf37790401c993a9aa11fb8dba97d65b73052e
Demsi <wojdems@gmail.com>
2023-02-01 19:10:51 +0100

    changed div ID name

--- script.js
+++ script.js
@@ -2,34 +2,34 @@
 let $number1=document.getElementById(`number1`)
 let $number2=document.getElementById(`number2`)
 let $resultbox=document.getElementById(`result`)
-let $aaa=document.getElementById(`aaa`)
+let $mathContainer=document.getElementById(`mathContainer`)
 
 
 function plus() {
     $resultbox.value=parseInt($number1.value) + parseInt($number2.value)
-    $aaa.innerHTML+=`<p></p>` + $number1.value + `+` + $number2.value + `=` + $resultbox.value
+    $mathContainer.innerHTML+=`<p></p>` + $number1.value + `+` + $number2.value + `=` + $resultbox.value
     updateLocalStorage($number1.value + `+` + $number2.value + `=` + $resultbox.value)
 }
 
 function minus() {
     $resultbox.value=($number1.value) - ($number2.value)
-    $aaa.innerHTML+=`<p></p>` + $number1.value + `-` + $number2.value + `=` + $resultbox.value
+    $mathContainer.innerHTML+=`<p></p>` + $number1.value + `-` + $number2.value + `=` + $resultbox.value
     updateLocalStorage($number1.value + `-` + $number2.value + `=` + $resultbox.value)
 }
 
 function divide() {
     if (parseInt($number2.value)===0){
-        $aaa.innerHTML+=`<p>You can't divide by 0!</p>`
+        $mathContainer.innerHTML+=`<p>You can't divide by 0!</p>`
     } else {
         $resultbox.value=parseInt($number1.value) / parseInt($number2.value)
-        $aaa.innerHTML+=`<p></p>` + $number1.value + `/` + $number2.value + `=` + $resultbox.value
+        $mathContainer.innerHTML+=`<p></p>` + $number1.value + `/` + $number2.value + `=` + $resultbox.value
         updateLocalStorage($number1.value + `/` + $number2.value + `=` + $resultbox.value)
     }
 }
 
 function multipla() {  
     $resultbox.value=parseInt($number1.value) * parseInt($number2.value)
-    $aaa.innerHTML+=`<p></p>` + $number1.value + `*` + $number2.value + `=` + $resultbox.value
+    $mathContainer.innerHTML+=`<p></p>` + $number1.value + `*` + $number2.value + `=` + $resultbox.value
     updateLocalStorage($number1.value + `*` + $number2.value + `=` + $resultbox.value)
 }
 
@@ -52,6 +52,6 @@ if(localStorage.getItem(`magazyn`)) {
     let actualElements = JSON.parse(localStorage.getItem(`magazyn`))
     
     for(let i=0; i<actualElements.length; i++) {
-        $aaa.innerHTML += `<p>` + actualElements[i] + `</p>`
+        $mathContainer.innerHTML += `<p>` + actualElements[i] + `</p>`
     }
 }
